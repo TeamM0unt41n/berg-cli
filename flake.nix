923254e048c2d35fead883576ed461b66f987998
Carlo Field <carlo@swiss.dev>
2025-10-21 09:53:18 +0200

    chore: repo maintenance (#5)
    * fix: add dependabot
    
    Signed-off-by: Carlo Field <carlo@swiss.dev>
    
    * feat: add flake
    
    ---------
    
    Signed-off-by: Carlo Field <carlo@swiss.dev>

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "berg-cli nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        overlays = [ (import rust-overlay )];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            pkg-config
+            openssl
+          ];
+        };
+
+        packages.default = (pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        }).buildRustPackage {
+          pname = "berg-cli";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ pkgs.openssl ];
+        };
+      }
+    );
+}
